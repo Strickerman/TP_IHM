77791758daca20051b5faee9c4095159eea635d3
Martin-B74 <martin.baratte74@gmail.com>
2025-12-16 11:50:58 +0100

    Adding gitignore file and a patch for a canvas display issue

--- Curve/src/main/java/ensisa/CurveApplication.java
+++ Curve/src/main/java/ensisa/CurveApplication.java
@@ -11,7 +11,7 @@ public class CurveApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CurveApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 280);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TP 2025");
         stage.setScene(scene);
         stage.show();
